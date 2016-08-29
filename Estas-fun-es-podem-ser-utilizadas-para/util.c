39233b2255697911b8a6844e53c6810367dd3626
Raoni Florentino da Silva Teixeira <raonifst@gmail.com>
2016-08-28 22:10:04 -0400

    Corrigindo um erro de memória

--- Estas-fun-es-podem-ser-utilizadas-para/util.c
+++ Estas-fun-es-podem-ser-utilizadas-para/util.c
@@ -18,20 +18,21 @@ e a função remover_espaco deve ser utilizada em cada iteração produzida pela
 */
 void remover_espaco(char *aux) {
 	int k;
-	if(aux[0] == ' ')
+	if(aux[0] == ' ') {
 		for(k = 1; aux[k] != '\0'; k++)
 			aux[k-1] = aux[k];
 		aux[k-1] = '\0';
+	}
 }
 
 void limpar_cadeia_caracteres(char *aux)
 {
 	int k, t;
-	if(aux[0] == ' ')
+	if(aux[0] == ' ') {
 		for(k = 1; aux[k] != '\0'; k++)
 			aux[k-1] = aux[k];
 		aux[k-1] = '\0';
-
+	}
 	for(t = 0; aux[t] != '\0'; t++) ;		
 	while( (aux[t] == '\0' || aux[t] == '\n' || aux[t] == '\r') && t > 0) t--;
 	aux[t+1] = '\0';
